820925dcae51d77a8af26f7014f19c469bb59cd6
Kevin Gasca <gasca.kevin.a@gmail.com>
2021-05-22 18:09:09 -0700

    Base Update
    * Added Header and Footer sections of base.
    * Added several CSS classes.

--- mbuster/routes.py
+++ mbuster/routes.py
@@ -16,7 +16,21 @@ def register():
         return redirect(url_for('index'))
     return render_template("register.html", title="Register", form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/about")
+def about_us():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("about.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")

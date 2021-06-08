7d23423e332d5b2af6a8cdb06663d4b3f514d5cf
Kevin Gasca <gasca.kevin.a@gmail.com>
2021-06-07 19:38:33 -0700

    About Us
    * Update app.run() command

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from sqlalchemy_utils import database_exists
 if __name__ == "__main__":
     if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
         db.create_all()
-    app.run()
+    app.run(host='0.0.0.0', port=5000)

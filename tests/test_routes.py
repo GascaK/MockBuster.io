5590d9777805ed56a5be34805b31c6d11185e9fa
Kevin Gasca <gasca.kevin.a@gmail.com>
2021-08-13 10:39:58 -0700

    Basic Testing
    * Added some API test cases.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,13 +1,10 @@
-import unittest
-from mbuster import app
+import requests
 
-class TestRoutes(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
+def test_get_user_info_check_status_code_equals_200():
+    response = requests.get("http://localhost:5000/api/draftjoker2?key=_R5mj3woyovq1z3J12sktw")
+    assert response.status_code == 200
 
-    def tearDown(self):
-        pass
-
-    def test_routes_index(self):
-        pass
-    
+def test_get_user_info_check_json_response_schema():
+    response = requests.get("http://localhost:5000/api/draftjoker2?key=_R5mj3woyovq1z3J12sktw")
+    body = response.json()
+    assert body["draftjoker2"]["API Key"] == "_R5mj3woyovq1z3J12sktw"
